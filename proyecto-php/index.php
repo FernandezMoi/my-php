12b58fde8d780058beb815b7d01da0f82685a16f
FernandezMoi <moibetico@hotmail.e>
2023-02-27 16:54:59 +0100

    february

---
+++ proyecto-php/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Videojuegos</title>
+</head>
+
+<body>
+    <!-- HEADER -->
+    <header>
+        <div id="logo.jpg">
+            <a href="index.php">
+                Blog de videojuegos
+            </a>
+        </div>
+
+        <!-- MENU -->
+
+        <nav>
+            <ul>
+                <li>
+                    <a href="index.php">Inicio</a>
+                </li>
+                <li>
+                    <a href="">Categoría 1</a>
+                </li>
+                <li>
+                    <a href="">Categoría 2</a>
+                </li>
+                <li>
+                    <a href="">Categoría 3</a>
+                </li>
+                <li>
+                    <a href="">Sobre mí</a>
+                </li>
+                <li>
+                    <a href="">Contacto</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+
+
+    <!-- SIDEBAR -->
+
+    <!-- CAJA PRINCIPAL -->
+
+    <!-- FOOTER -->
+
+</body>
+
+</html>
